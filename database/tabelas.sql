03782224b1308f8c68974d95fdb12ccd1156e0c8
mathznxs.dev <math983839179@gmail.com>
2024-10-16 14:04:29 -0300

    1

--- database/tabelas.sql
+++ database/tabelas.sql
@@ -1,5 +1,5 @@
 -- Criar tabela Usuarios
-CREATE TABLE usuarios (
+CREATE TABLE Usuarios (
     id_usuario INT PRIMARY KEY IDENTITY(1,1),
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100)  UNIQUE NOT NULL,
@@ -16,3 +16,17 @@ CREATE TABLE Contas (
     data_cadastro DATETIME DEFAULT GETDATE(),
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
+
+-- Criar tabela Transações
+CREATE TABLE Transacoes (
+    id_transacao INT PRIMARY KEY IDENTITY(1,1),
+    id_usuario INT,
+    id_conta INT,
+    valor DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(50),
+    tipo_transacao CHAR(7),
+    descricao VARCHAR(255),
+    data_transacao DATETIME DEFAULT GETDATE(),
+);
+
+-- Criar tabela orçamentos
